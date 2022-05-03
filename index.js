f09037b9ba8df072f64e9367828a3c55e4e13958
root2 <2rootcom4@gmail.com>
2022-05-03 10:49:20 +0900

    refactor: change routers folder into routes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const mainRouter = require('./routers/main');
-const ansiWebRouter = require('./routers/ansi-web');
+const mainRouter = require('./routes/main');
+const ansiWebRouter = require('./routes/ansi-web');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
