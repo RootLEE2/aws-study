d9de864e2bda6d775f1ee2de03ac1b9cbc43cb79
root2 <2rootcom4@gmail.com>
2022-05-03 12:08:39 +0900

    feat: add /ansi/web-study/hw-0429

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
-const FileStore = require('')
 const app = express();
 const port = 3000;
 const fileStoreOptions = {};
--- routes/ansi-web.js
+++ routes/ansi-web.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const router = express.Router();
 
 // /ansi/web-study
@@ -10,5 +11,22 @@ router.get('/', (req, res, next) => {
 router.get('/hw-0415', (req, res, next) => {
   res.render('ansi/web-study/hw-0415.html');
 });
+
+// /ansi/web-study/hw-0429
+router.get('/hw-0429', (req, res, next) => {
+  const session = req.session;
+
+  res.render('ansi/web-study/hw-0429.html', { justText: session.justText });
+});
+
+router.post('/hw-0429', (req, res, next) => {
+  const { just_text } = req.body;
+
+  req.session.justText = just_text;
+
+  req.session.save(() => {
+    res.redirect('./hw-0429');
+  });
+})
 
 module.exports = router;
