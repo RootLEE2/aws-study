11bded59ede43e8b9f1eafcaaf4add5acb8d6727
root2 <2rootcom4@gmail.com>
2022-05-03 10:45:10 +0900

    style: change quotes

--- routers/main.js
+++ routers/main.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.render("main.html");
+router.get('/', (req, res, next) => {
+  res.render('main.html');
 })
 
 router.get('/ansi', (req, res, next) => {
